1ea14af688e7c9ddd433e85df10fb140854e0819
Hou Yin <houyiny@gmail.com>
2025-05-05 00:03:21 +0800

    Add basic 3D objects: Cube, Sphere, Torus

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 import * as THREE from 'three'
 
 
+// === Constants ===
+const COLORS = {
+    red: 0xFF0000,
+    green: 0x00FF00,
+    blue: 0x0000FF
+};
+
 // === Globals ===
 let scene, camera, renderer;
 
@@ -11,7 +18,7 @@ function initScene() {
     
     // then a camera
     camera = new THREE.PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 1000)
-    camera.position.z = 10;
+    camera.position.z = 25;
     
     // and a renderer, which renders the scene
     renderer = new THREE.WebGLRenderer();
@@ -27,6 +34,36 @@ function onWindowResize() {
     renderer.setSize(innerWidth, innerHeight);
 }
 
+// === Objects ===
+function createCube() {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: COLORS.red });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+}
+
+function createSphere() {
+    const geometry = new THREE.SphereGeometry(5, 16, 16);
+    const material = new THREE.MeshBasicMaterial({ color: COLORS.green });
+    const sphere = new THREE.Mesh(geometry, material);
+    sphere.position.x = 10;
+    scene.add(sphere)
+}
+
+function createDonut() {
+    const geometry = new THREE.TorusGeometry(5, 3, 16, 100);
+    const material = new THREE.MeshBasicMaterial({ color: COLORS.blue });
+    const torus = new THREE.Mesh(geometry, material);
+    torus.position.x = -10;
+    scene.add(torus);
+}
+
+function createObjects() {
+    createCube();
+    createSphere();
+    createDonut();
+}
+
 // === Animation Loop ===
 function animate() {
     renderer.setAnimationLoop(() => {
@@ -36,6 +73,7 @@ function animate() {
 
 function main() {
     initScene();
+    createObjects();
     animate();
 }
 main()
